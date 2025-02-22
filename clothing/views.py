5e607fd03472ff068dafa6db4ed01d00f41b271b
Vaibhav Shewale <itami-macbook@Vaibhavs-MacBook-Pro.local>
2025-02-22 14:14:36 +0530

    2nd commit

--- clothing/views.py
+++ clothing/views.py
@@ -1,6 +1,25 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 from clothing.models import Product
-# Create your views here.
+from .forms import ProductForm
+
 def clothing(request):
     pro_image = Product.objects.all()
-    return render(request, 'clothing/fashion.html', {'pro_img':pro_image})
+    form = ProductForm()
+
+    if request.method == "POST" and request.headers.get("X-Requested-With") == "XMLHttpRequest":
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            product = form.save()
+            return JsonResponse({
+                "success": True,
+                "name": product.name,
+                "size": product.size,
+                "colour": product.colour,
+                "price": product.price,
+                "image_url": product.image.url
+            })
+        else:
+            return JsonResponse({"success": False, "errors": form.errors})
+
+    return render(request, "clothing/fashion.html", {'pro_img': pro_image, 'form': form})
