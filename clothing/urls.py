6fb158488f2a28abb8f1a80b2bfa6373d7b64bb0
Vaibhav Shewale <itami-macbook@Vaibhavs-MacBook-Pro.local>
2025-02-24 14:23:24 +0530

    3rd commit

--- clothing/urls.py
+++ clothing/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path("", views.clothing, name="clothing"), 
+    path("", views.clothing, name="clothing"),  
+    path("edit/<int:product_id>/", views.edit_product, name="edit_product"),
+    path("delete/<int:product_id>/", views.delete_product, name="delete_product"),
 ]
--- clothing/views.py
+++ clothing/views.py
@@ -1,5 +1,5 @@
 from django.http import JsonResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from clothing.models import Product
 from .forms import ProductForm
 
@@ -23,3 +23,22 @@ def clothing(request):
             return JsonResponse({"success": False, "errors": form.errors})
 
     return render(request, "clothing/fashion.html", {'pro_img': pro_image, 'form': form})
+
+
+def edit_product(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    
+    if request.method == "POST":
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            return redirect("clothing")  # Redirect to the product list
+
+    form = ProductForm(instance=product)
+    return render(request, "clothing/edit_product.html", {"form": form, "product": product})
+
+
+def delete_product(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    product.delete()
+    return redirect("clothing")
